7fd504f7c2375d8664cd1fa113c671d9dd2ff416
Matyasov.Mixail <matyasov.mixail@gmail.com>
2018-04-07 13:07:57 +0300

    1

--- modules/complex-number/test/test_matyasov_mixail_complex_number.cpp
+++ modules/complex-number/test/test_matyasov_mixail_complex_number.cpp
@@ -1,19 +1,18 @@
-// Copyright 2018 Matyasov Mixail
+// Copyright 2018 Okunev Boris
 
 #include <gtest/gtest.h>
 
 #include "include/complex_number.h"
 
-TEST(Matyasov_Mixail_ComplexNumberTest, can_copy_complex_number)
-{
-    // Arrange
-    double re = 1.0;
-    double im = 2.0;
+TEST(Okunev_Boris_ComplexNumberTest, Two_Same_Complex_Are_Equal) {
+	// Arrange
+	double re = 1.0;
+	double im = 2.0;
 
-    // Act
-    ComplexNumber z(re, im);
-    ComplexNumber z1(re, im);
+	// Act
+	ComplexNumber z(re, im);
+	ComplexNumber z1(re, im);
 
-    // Assert
-    EXPECT_TRUE(z == z1);
+	// Assert
+	EXPECT_TRUE(z == z1);
 }
