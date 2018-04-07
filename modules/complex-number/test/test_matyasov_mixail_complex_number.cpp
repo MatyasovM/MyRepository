6551a07e138478367fc4da56286aead6dd8c884a
Matyasov.Mixail <matyasov.mixail@gmail.com>
2018-04-07 12:46:54 +0300

    1

--- modules/complex-number/test/test_matyasov_mixail_complex_number.cpp
+++ modules/complex-number/test/test_matyasov_mixail_complex_number.cpp
@@ -6,44 +6,8 @@
 
 TEST(Matyasov_Mixail_ComplexNumberTest, can_copy_complex_number)
 {
-	ComplexNumber number(2, 1);
-	ComplexNumber copNumber(number);
+    ComplexNumber num1(1.0, 2.0);
+    ComplexNumber num2(1.0, 2.0);
 
-	EXPECT_EQ(number, copNumber);
-}
-
-TEST(Matyasov_Mixail_ComplexNumberTest, can_plus_negative_number)
-{
-	ComplexNumber number(-2, -1);
-	ComplexNumber copNumber(-3, -1);
-
-	EXPECT_EQ(number + copNumber, *(new ComplexNumber(-5, -2)));
-}
-
-TEST(Matyasov_Mixail_ComplexNumberTest, can_compare_opposite_number)
-{
-	ComplexNumber number(-2, -1);
-	ComplexNumber copNumber(-1, -2);
-
-	EXPECT_NE(number, copNumber);
-}
-
-TEST(Matyasov_Mixail_ComplexNumberTest, base_number_and_copy_is_not_same_object)
-{
-	ComplexNumber number(-2, -1);
-	ComplexNumber copNumber(number);
-	number.setIm(5);
-
-	EXPECT_NE(number.getIm(), copNumber.getIm());
-}
-
-TEST(Matyasov_Mixail_ComplexNumberTest, multyple_do_not_change_base_number)
-{
-	ComplexNumber number(-2, -1);
-	ComplexNumber copNumber(number);
-	ComplexNumber buf(number);
-	ComplexNumber result = number + copNumber;
-
-	EXPECT_EQ(number, buf);
-	EXPECT_EQ(copNumber, buf);
+    EXPECT_EQ(num1, num2);
 }
